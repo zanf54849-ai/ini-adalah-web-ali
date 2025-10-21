d56ff906e955042a0d1ceee0b07ffa1429fa3a0c
aku adalah ali <zanf54849@gmail.com>
2025-10-21 11:08:37 +0700

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- shop2.blade.php
+++ shop2.blade.php
@@ -238,44 +238,44 @@
                                 </div>
                             </div>-->
                             <a href="{{ route('contact') }}" class="nav-item nav-link">Contact</a>
-                            <div class="nav-item dropdown d-block d-lg-none mb-3">
-                                <a href="#" class="nav-link" data-bs-toggle="dropdown"><span class="dropdown-toggle">All
-                                        Category</span></a>
-                                <div class="dropdown-menu m-0">
-                                    <ul class="list-unstyled categories-bars">
-                                        <li>
-                                            <div class="categories-bars-item">
-                                                <a href="#">Accessories</a>
-                                                <span>(3)</span>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="categories-bars-item">
-                                                <a href="#">Electronics & Computer</a>
-                                                <span>(5)</span>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="categories-bars-item">
-                                                <a href="#">Laptops & Desktops</a>
-                                                <span>(2)</span>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="categories-bars-item">
-                                                <a href="#">Mobiles & Tablets</a>
-                                                <span>(8)</span>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="categories-bars-item">
-                                                <a href="#">SmartPhone & Smart TV</a>
-                                                <span>(5)</span>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+                            <!--<div class="nav-item dropdown d-block d-lg-none mb-3">-->
+                            <!--    <a href="#" class="nav-link" data-bs-toggle="dropdown"><span class="dropdown-toggle">All-->
+                            <!--            Category</span></a>-->
+                            <!--    <div class="dropdown-menu m-0">-->
+                            <!--        <ul class="list-unstyled categories-bars">-->
+                            <!--            <li>-->
+                            <!--                <div class="categories-bars-item">-->
+                            <!--                    <a href="#">Accessories</a>-->
+                            <!--                    <span>(3)</span>-->
+                            <!--                </div>-->
+                            <!--            </li>-->
+                            <!--            <li>-->
+                            <!--                <div class="categories-bars-item">-->
+                            <!--                    <a href="#">Electronics & Computer</a>-->
+                            <!--                    <span>(5)</span>-->
+                            <!--                </div>-->
+                            <!--            </li>-->
+                            <!--            <li>-->
+                            <!--                <div class="categories-bars-item">-->
+                            <!--                    <a href="#">Laptops & Desktops</a>-->
+                            <!--                    <span>(2)</span>-->
+                            <!--                </div>-->
+                            <!--            </li>-->
+                            <!--            <li>-->
+                            <!--                <div class="categories-bars-item">-->
+                            <!--                    <a href="#">Mobiles & Tablets</a>-->
+                            <!--                    <span>(8)</span>-->
+                            <!--                </div>-->
+                            <!--            </li>-->
+                            <!--            <li>-->
+                            <!--                <div class="categories-bars-item">-->
+                            <!--                    <a href="#">SmartPhone & Smart TV</a>-->
+                            <!--                    <span>(5)</span>-->
+                            <!--                </div>-->
+                            <!--            </li>-->
+                            <!--        </ul>-->
+                            <!--    </div>-->
+                            <!--</div>-->
                         </div>
 <a href="https://wa.me/628118128571" target="_blank"
                             class="btn btn-secondary rounded-pill py-2 px-4 px-lg-3 mb-3 mb-md-3 mb-lg-0">
@@ -1138,7 +1138,7 @@
                                         <a href="{{ route('shop') }}" class="rounded">1</a>
                                         <a href="{{ route('shop2') }}" class="active rounded">2</a>
                                         <a href="{{ route('shop3') }}" class="rounded">3</a>
-                                        <a href="{{ route('shop') }}?page=4" class="rounded">4</a>
+                                        <a href="{{ route('shop4') }}" class="rounded">4</a>
                                         <a href="{{ route('shop') }}?page=5" class="rounded">5</a>
                                         <a href="{{ route('shop') }}?page=6" class="rounded">6</a>
                                         <a href="{{ route('shop3') }}" class="rounded">&raquo;</a>
